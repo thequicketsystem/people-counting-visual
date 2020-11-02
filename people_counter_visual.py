bb46946485cabf8266d00f5026d2a63abab244e9
ggasmithh <ggasmithh@users.noreply.github.com>
2020-11-01 20:37:47 -0600

    Removed unused stuff

--- people_counter_visual.py
+++ people_counter_visual.py
@@ -1,9 +1,13 @@
-# From: https://www.pyimagesearch.com/2018/08/13/opencv-people-counter/
-
 # USAGE
 # To read and write back out to video:
-# python3 people_counter_visual.py --prototxt mobilenet_ssd/MobileNetSSD_deploy.prototxt \
-#	--model mobilenet_ssd/MobileNetSSD_deploy.caffemodel 
+# python people_counter.py --prototxt mobilenet_ssd/MobileNetSSD_deploy.prototxt \
+#	--model mobilenet_ssd/MobileNetSSD_deploy.caffemodel --input videos/example_01.mp4 \
+#	--output output/output_01.avi
+#
+# To read from webcam and write back out to disk:
+# python people_counter.py --prototxt mobilenet_ssd/MobileNetSSD_deploy.prototxt \
+#	--model mobilenet_ssd/MobileNetSSD_deploy.caffemodel \
+#	--output output/webcam_output.avi
 
 # import the necessary packages
 from pyimagesearch.centroidtracker import CentroidTracker
@@ -23,6 +27,10 @@ ap.add_argument("-p", "--prototxt", required=True,
 	help="path to Caffe 'deploy' prototxt file")
 ap.add_argument("-m", "--model", required=True,
 	help="path to Caffe pre-trained model")
+#ap.add_argument("-i", "--input", type=str,
+#	help="path to optional input video file")
+#ap.add_argument("-o", "--output", type=str,
+#	help="path to optional output video file")
 ap.add_argument("-c", "--confidence", type=float, default=0.4,
 	help="minimum probability to filter weak detections")
 ap.add_argument("-s", "--skip-frames", type=int, default=30,
@@ -67,13 +75,14 @@ fps = FPS().start()
 
 # loop over frames from the video stream
 while True:
-	# grab the next frame from VideoStream
+	# grab the next frame and handle if we are reading from either
+	# VideoCapture or VideoStream
 	frame = vs.read()
 
-	# resize the frame to have a maximum width of 256 pixels (the
+	# resize the frame to have a maximum width of 500 pixels (the
 	# less data we have, the faster we can process it), then convert
 	# the frame from BGR to RGB for dlib
-	frame = imutils.resize(frame, width=256)
+	frame = imutils.resize(frame, width=500)
 	rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
 	# if the frame dimensions are empty, set them
@@ -166,6 +175,8 @@ while True:
 	# centroids with (2) the newly computed object centroids
 	objects = ct.update(rects)
 
+	currentlyInFrame = 0
+
 	# loop over the tracked objects
 	for (objectID, centroid) in objects.items():
 		# check to see if a trackable object exists for the current
@@ -179,29 +190,7 @@ while True:
 		# otherwise, there is a trackable object so we can utilize it
 		# to determine direction
 		else:
-			# the difference between the y-coordinate of the *current*
-			# centroid and the mean of *previous* centroids will tell
-			# us in which direction the object is moving (negative for
-			# 'up' and positive for 'down')
-			y = [c[1] for c in to.centroids]
-			direction = centroid[1] - np.mean(y)
-			to.centroids.append(centroid)
-
-			# check to see if the object has been counted or not
-			if not to.counted:
-				# if the direction is negative (indicating the object
-				# is moving up) AND the centroid is above the center
-				# line, count the object
-				if direction < 0 and centroid[1] < H // 2:
-					totalUp += 1
-					to.counted = True
-
-				# if the direction is positive (indicating the object
-				# is moving down) AND the centroid is below the
-				# center line, count the object
-				elif direction > 0 and centroid[1] > H // 2:
-					totalDown += 1
-					to.counted = True
+			currentlyInFrame += 1
 
 		# store the trackable object in our dictionary
 		trackableObjects[objectID] = to
@@ -213,35 +202,15 @@ while True:
 			cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 		cv2.circle(frame, (centroid[0], centroid[1]), 4, (0, 255, 0), -1)
 
-	# construct a tuple of information we will be displaying on the
-	# frame
-	info = [
-		("Up", totalUp),
-		("Down", totalDown),
-		("Status", status),
-	]
+	cv2.putText(frame, f"count: {currentlyInFrame}", (10, H - 80), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2) 
 
-	# loop over the info tuples and draw them on our frame
-	for (i, (k, v)) in enumerate(info):
-		text = "{}: {}".format(k, v)
-		cv2.putText(frame, text, (10, H - ((i * 20) + 20)),
-			cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
-
-	# show the output frame
-	cv2.imshow("Frame", frame)
-	key = cv2.waitKey(1) & 0xFF
-
-	# if the `q` key was pressed, break from the loop
-	if key == ord("q"):
-		break
-
-	# increment the total number of frames processed thus far and
-	# then update the FPS counter
 	totalFrames += 1
 	fps.update()
 
+
 # stop the timer and display FPS information
 fps.stop()
+
 print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
 print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
 
