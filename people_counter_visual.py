208b2ff8f094901a6d51e2e01c34ff5ed77827eb
Garrett Smith <ggasmithh@users.noreply.github.com>
2020-11-09 20:27:11 -0600

    Reintroduced option for video file input

--- people_counter_visual.py
+++ people_counter_visual.py
@@ -27,10 +27,10 @@ ap.add_argument("-p", "--prototxt", required=True,
 	help="path to Caffe 'deploy' prototxt file")
 ap.add_argument("-m", "--model", required=True,
 	help="path to Caffe pre-trained model")
-#ap.add_argument("-i", "--input", type=str,
-#	help="path to optional input video file")
-#ap.add_argument("-o", "--output", type=str,
-#	help="path to optional output video file")
+ap.add_argument("-i", "--input", type=str,
+	help="path to optional input video file")
+ap.add_argument("-o", "--output", type=str,
+	help="path to optional output video file")
 ap.add_argument("-c", "--confidence", type=float, default=0.4,
 	help="minimum probability to filter weak detections")
 ap.add_argument("-s", "--skip-frames", type=int, default=30,
@@ -48,9 +48,18 @@ CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
 print("[INFO] loading model...")
 net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
 
-print("[INFO] starting video stream...")
-vs = VideoStream(src=0).start()
-time.sleep(2.0)
+# if a video path was not supplied, grab a reference to the webcam
+if not args.get("input", False):
+	print("[INFO] starting video stream...")
+	vs = VideoStream(src=0).start()
+	time.sleep(2.0)
+# otherwise, grab a reference to the video file
+else:
+	print("[INFO] opening video file...")
+	vs = cv2.VideoCapture(args["input"])
+
+# initialize the video writer (we'll instantiate later if need be)
+writer = None
 
 # initialize the frame dimensions (we'll set them as soon as we read
 # the first frame from the video)
@@ -67,8 +76,6 @@ trackableObjects = {}
 # initialize the total number of frames processed thus far, along
 # with the total number of objects that have moved either up or down
 totalFrames = 0
-totalDown = 0
-totalUp = 0
 
 # start the frames per second throughput estimator
 fps = FPS().start()
@@ -78,6 +85,12 @@ while True:
 	# grab the next frame and handle if we are reading from either
 	# VideoCapture or VideoStream
 	frame = vs.read()
+	frame = frame[1] if args.get("input", False) else frame
+	
+	# if we are viewing a video and we did not grab a frame then we
+	# have reached the end of the video
+	if args["input"] is not None and frame is None:
+		break
 
 	# resize the frame to have a maximum width of 500 pixels (the
 	# less data we have, the faster we can process it), then convert
@@ -88,6 +101,13 @@ while True:
 	# if the frame dimensions are empty, set them
 	if W is None or H is None:
 		(H, W) = frame.shape[:2]
+
+	# if we are supposed to be writing a video to disk, initialize
+	# the writer
+	if args["output"] is not None and writer is None:
+		fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+		writer = cv2.VideoWriter(args["output"], fourcc, 30,
+			(W, H), True)
 
 	# initialize the current status along with our list of bounding
 	# box rectangles returned by either (1) our object detector or
@@ -204,6 +224,21 @@ while True:
 
 	cv2.putText(frame, f"count: {currentlyInFrame}", (10, H - 80), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2) 
 
+	
+	# check to see if we should write the frame to disk
+	if writer is not None:
+		writer.write(frame)
+		
+	# show the output frame
+	cv2.imshow("Frame", frame)
+	
+	# if the `q` key was pressed, break from the loop
+	key = cv2.waitKey(1) & 0xFF
+	if key == ord("q"):
+		break
+		
+	# increment the total number of frames processed thus far and
+	# then update the FPS counter
 	totalFrames += 1
 	fps.update()
 
@@ -214,7 +249,17 @@ fps.stop()
 print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
 print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
 
-vs.release()
+# check to see if we need to release the video writer pointer
+if writer is not None:
+	writer.release()
+# if we are not using a video file, stop the camera video stream
+if not args.get("input", False):
+	vs.stop()
+# otherwise, release the video file pointer
+else:
+	vs.release()
+# close any open windows
+cv2.destroyAllWindows()
 
 # close any open windows
 cv2.destroyAllWindows()
